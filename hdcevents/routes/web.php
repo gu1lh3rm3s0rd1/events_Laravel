49d1aedba99eb761be3bc7787868bbc756accf62
Guilherme-Sordi-Ti <gsordi123@outlook.com>
2024-01-25 10:45:15 -0300

    update rotas web

--- hdcevents/routes/web.php
+++ hdcevents/routes/web.php
@@ -18,6 +18,11 @@ use Illuminate\Support\Facades\Route;
 //     return view('welcome');
 // });
 
+// index padrao de acesso para os registros
 Route::get('/', [EventsController::class, 'index']);
+// create padrao de acesso para criar componentes no banco
 Route::get('/events/create', [EventsController::class, 'create']);
+// show padrao de acesso para exibir dados especificos
+Route::get('/events/{id}', [EventsController::class, 'show']);
+// store padrao de acesso para enviar / registrar os dados no banco
 Route::post('/events', [EventsController::class, 'store']);
